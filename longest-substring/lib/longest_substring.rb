47126598eddb480c445025fe975e950885bec2ad
Evan Wheeler <anon0mys87@gmail.com>
2018-08-07 23:32:23 -0600

    Refactor to longest substring method to pull out substring createion and check letter methods.

--- longest-substring/lib/longest_substring.rb
+++ longest-substring/lib/longest_substring.rb
@@ -2,18 +2,25 @@
 # @return {Integer}
 
 def length_of_longest_substring(s)
-  substrings = []
-  queue = []
-  letters = s.chars
-  return 1 if letters.length == 1
-  letters.each do |letter|
-    if queue.include?(letter)
-      substrings << queue.join('')
-      queue = [letter]
-    else
-      queue.push(letter)
-    end
-  end
-  longest = substrings.max_by { |string| string.length }
+  return 1 if s.length == 1
+  create_substrings(s.chars)
+  longest = @substrings.max_by { |string| string.length }
   longest.length
+end
+
+def create_substrings(characters)
+  @substrings = []
+  @queue = []
+  characters.each do |character|
+    check_letter(character)
+  end
+end
+
+def check_letter(letter)
+  if @queue.include?(letter)
+    @substrings << @queue.join('')
+    @queue = [letter]
+  else
+    @queue.push(letter)
+  end
 end
